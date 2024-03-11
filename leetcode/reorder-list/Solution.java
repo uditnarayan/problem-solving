0176ea2b83dc9e3ff60e0052113d1828ff1e4d25
Udit Narayan <1077880+uditnarayan@users.noreply.github.com>
2024-03-11 22:14:20 +0530

    leetcode: reorder-list

---
+++ leetcode/reorder-list/Solution.java
@@ -0,0 +1,57 @@
+// https://leetcode.com/problems/reorder-list/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null) return;
+
+        ListNode middle = findMiddle(head);
+        ListNode reversed = reverse(middle);
+        merge(head, reversed);
+    }
+
+    public ListNode findMiddle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public ListNode reverse(ListNode head) {
+        ListNode current = head;
+        ListNode prev = null;
+        ListNode next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+
+    public void merge(ListNode head1, ListNode head2) {
+        ListNode temp = null;
+        while (head2.next != null) {
+            temp = head1.next;
+            head1.next = head2;
+            head1 = temp;
+
+            temp = head2.next;
+            head2.next = head1;
+            head2 = temp;
+        }
+    }
+}
